92679b39bb547eeb6aa79d2064bbf4c4e417490e
Faisal-hameed82 <faisalfrontend@gmail.com>
2024-08-19 14:45:06 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,84 @@
+function showCardDog1() {
+    document.getElementById('dog1').innerHTML = /* HTML*/ `
+    <img class="img-show" src="https://getacademy.github.io/PictureLottox/img/dog.jpg" />
+    `;
+}
+
+function showCardFrog1() {
+    document.getElementById('frog1').innerHTML = /* HTML*/ `
+    <img class="img-show" src="https://getacademy.github.io/PictureLottox/img/frog.jpg" />
+    `;
+}
+function showCardHorse1() {
+    document.getElementById('horse1').innerHTML = /* HTML*/ `
+      <img class="img-show" src="https://getacademy.github.io/PictureLottox/img/horse.jpg" />
+      `;
+}
+function showCardChicken1() {
+    document.getElementById('chicken1').innerHTML = /* HTML*/ `
+          <img class="img-show" src="https://getacademy.github.io/PictureLottox/img/chicken.jpg" />
+          `;
+}
+function showCardSpider1() {
+    document.getElementById('spider1').innerHTML = /* HTML*/ `
+              <img class="img-show" src="https://getacademy.github.io/PictureLottox/img/spider.jpg" />
+              `;
+}
+function showCardFrog2() {
+    document.getElementById('frog2').innerHTML = /* HTML*/ `
+                  <img class="img-show" src="https://getacademy.github.io/PictureLottox/img/frog.jpg" />
+                  `;
+}
+function showCardSpider2() {
+    document.getElementById('spider2').innerHTML = /* HTML*/ `
+                      <img class="img-show" src="https://getacademy.github.io/PictureLottox/img/spider.jpg" />
+                      `;
+}
+function showCardTiger1() {
+    document.getElementById('tiger1').innerHTML = /* HTML*/ `
+                          <img class="img-show" src="https://getacademy.github.io/PictureLottox/img/tiger.jpg" />
+                          `;
+}
+function showCardChicken2() {
+    document.getElementById('chicken2').innerHTML = /* HTML*/ `
+                           <img class="img-show" src="https://getacademy.github.io/PictureLottox/img/chicken.jpg" />
+                            `;
+}
+
+function showCardDog2() {
+    document.getElementById('dog2').innerHTML = /* HTML*/ `
+                            <img class="img-show" src="https://getacademy.github.io/PictureLottox/img/dog.jpg" />
+                            `;
+}
+function showCardElephant1() {
+    document.getElementById('elephant1').innerHTML = /* HTML*/ `
+                              <img class="img-show" src="https://getacademy.github.io/PictureLottox/img/elephant.jpg" />
+                              `;
+}
+function showCardCat1() {
+    document.getElementById('cat1').innerHTML = /* HTML*/ `
+                                <img class="img-show" src="https://getacademy.github.io/PictureLottox/img/cat.gif" />
+                                `;
+}
+function showCardCat2() {
+    document.getElementById('cat2').innerHTML = /* HTML*/ `
+                                <img class="img-show" src="https://getacademy.github.io/PictureLottox/img/cat.gif" />
+                                `;
+}
+function showCardElephant2() {
+    document.getElementById('elephant2').innerHTML = /* HTML*/ `
+                                <img class="img-show" src="https://getacademy.github.io/PictureLottox/img/elephant.jpg" />
+                                `;
+}
+function showCardHorse2() {
+    document.getElementById('horse2').innerHTML = /* HTML*/ `
+                                <img class="img-show" src="https://getacademy.github.io/PictureLottox/img/horse.jpg" />
+                                `;
+}
+function showCardTiger2() {
+    document.getElementById('tiger2').innerHTML = /*HTML */ `
+                                <img class="img-show" src="https://getacademy.github.io/PictureLottox/img/tiger.jpg" />
+
+    `;
+}
+
